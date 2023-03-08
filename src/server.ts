a45d1921f8743b42999cb3d7be0667d688b17657
alexfarias7 <alexfarias44@gmail.com>
2023-03-08 03:02:30 -0300

    implement swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import Express from 'express';
+import swaggwerUi from 'swagger-ui-express';
 
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = Express();
 const port = 3333;
 
 app.use(Express.json());
+app.use('/api-docs', swaggwerUi.serve, swaggwerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(port, () => console.log('server is runnig'));
